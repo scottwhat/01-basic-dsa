a03383e027c2d2627541f82dd06496b2830f79f0
scott <scottcooperaus1992@gmail.com>
2023-11-08 19:46:31 +1100

    browser history

---
+++ doublylinkedlists/1472-design-browser-history.py
@@ -0,0 +1,35 @@
+
+class ListNode:
+    
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev 
+        self.next = next
+
+class BrowserHistory:
+    
+    def __init__(self, homepage: str) -> None:
+        #treat as head
+        self.cur = ListNode(homepage)
+    
+    def visit(self, url: str) -> None:
+        self.cur.next = ListNode(url, self.cur)
+        self.cur = self.cur.next 
+       
+    def back(self, steps: int) -> str:
+        #check self.cur true
+        #move through each one for the amount of steps
+        #self.cur.prev makes sure no null pointer 
+        while self.cur.prev and steps > 0:
+            self.cur = self.cur.prev
+            steps -= 1
+        return self.cur.val
+        
+        
+    def forward(self, steps: int) -> str:
+        while self.cur.next and steps > 0:
+            self.cur = self.cur.next
+            steps -=1
+        return self.cur.val 
+        
+       

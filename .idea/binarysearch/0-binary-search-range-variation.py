caf34ea181912f8a515dbdbbc72c86a712382dea
scott <scottcooperaus1992@gmail.com>
2023-11-12 14:38:04 +1100

    update

---
+++ .idea/binarysearch/0-binary-search-range-variation.py
@@ -0,0 +1,30 @@
+#finding 10 in array
+
+def binarySearch(low, high):
+    
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if isCorrect(mid) > 0:
+            
+            high = mid - 1
+        
+        elif isCorrect(mid) < 0:
+            low = mid + 1
+        
+        else: 
+            return mid
+        
+    return -1
+        
+
+
+def isCorrect(n):
+    if n > 10:
+        return 1
+    
+    if n < 10:
+        return -1
+    
+    else:
+        return 0

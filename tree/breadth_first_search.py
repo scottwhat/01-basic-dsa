e07a597f9681708039aba4d1eb4a8ff006d24bbe
scott <scottcooperaus1992@gmail.com>
2023-11-18 22:11:27 +1100

    comeon commit time

--- tree/breadth_first_search.py
+++ tree/breadth_first_search.py
@@ -1,3 +1,6 @@
+
+#use a queue to process each level of the tree. first in first out to get the left to right 
+
 from collections import deque
 
 class TreeNode:
@@ -7,21 +10,41 @@ class TreeNode:
         self.right = None
 
 
-#
 def bfs(root):
-    queue = deque()
+    queue = deuque()
 
     if root:
         queue.append(root)
-    
+        
+    #process each level
     level = 0
     while len(queue) > 0:
-        print("level: ", level)
+        print("Level: ", level)
         for i in range(len(queue)):
             curr = queue.popleft()
             print(curr.val)
+            
             if curr.left:
                 queue.append(curr.left)
             if curr.right:
                 queue.append(curr.right)
-        level += 1
+        level += 1;
+        
+# #
+# def bfs(root):
+#     queue = deque()
+
+#     if root:
+#         queue.append(root)
+    
+#     level = 0
+#     while len(queue) > 0:
+#         print("level: ", level)
+#         for i in range(len(queue)):
+#             curr = queue.popleft()
+#             print(curr.val)
+#             if curr.left:
+#                 queue.append(curr.left)
+#             if curr.right:
+#                 queue.append(curr.right)
+#         level += 1

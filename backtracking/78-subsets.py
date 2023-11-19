a9dd8900b08801fdcc4a71b4fc9ac6f1f3cf7a76
scott <scottcooperaus1992@gmail.com>
2023-11-19 15:30:59 +1100

    commy commit

--- backtracking/78-subsets.py
+++ backtracking/78-subsets.py
@@ -1,4 +1,5 @@
 # https://neetcode.io/courses/dsa-for-beginners/22
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
@@ -16,7 +17,17 @@ class Solution:
             dfs(i + 1)
             # decision NOT to include nums[i]
             subset.pop()
+            # q: what does SOLID stand for? 
+            
             dfs(i + 1)
 
         dfs(0)
         return res
+
+
+        # @copilot:what does solid stand for in programming?
+        
+    
+    
+    
+    
--- heap/0-Heap_class.py
+++ heap/0-Heap_class.py
@@ -1,53 +1,18 @@
-class MaxHeap:
-    def __init__(self):
-        self.heap = []
 
-    def _left_child(self, index):
-        return 2 * index + 1
 
-    def _right_child(self, index):
-        return 2 * index + 2
-
-    def _parent(self, index):
-        return (index - 1) // 2
-
-    def _swap(self, index1, index2):
-        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
-
-    def insert(self, value):
+class Heap:
+    
+    def init(self):
+        self.heap = [0]
+    
+    def push(self, value):
         self.heap.append(value)
-        current = len(self.heap) - 1
-
-        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
-            self._swap(current, self._parent(current))
-            current = self._parent(current)
-
-
-
-myheap = MaxHeap()
-myheap.insert(99)
-myheap.insert(72)
-myheap.insert(61)
-myheap.insert(58)
-
-print(myheap.heap)  
-
-
-myheap.insert(100)
-
-print(myheap.heap)  
-
-
-myheap.insert(75)
-
-print(myheap.heap)
-
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    [99, 72, 61, 58]
-    [100, 99, 61, 58, 72]
-    [100, 99, 75, 58, 72, 61]
-
-"""
+        i = len(self.heap) - 1 
+        
+        #percolate up
+    
+        while self.heap[i] < self.heap[i // 2]:
+            tmp = self.heap[i]
+            self.heap[i] = self.heap[i // 2]
+            self.heap [i // 2] = tmp    
+            i = i // 2

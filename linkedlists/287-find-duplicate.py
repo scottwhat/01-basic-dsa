bb3709217a0f36015a8eb88159a02c1804c8499a
scott <scottcooperaus1992@gmail.com>
2023-11-24 22:27:37 +1100

    commy

---
+++ linkedlists/287-find-duplicate.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/find-the-duplicate-number/
+
+# return the number thats a repeat in the list. only 1 repeat
+# could use hashset bnut thats o(n) time and memory 
+
+
+#linked list cycle problem
+#floys algorithm 
+ 

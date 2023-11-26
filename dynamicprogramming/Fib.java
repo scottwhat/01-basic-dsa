84c96015f657d7d317328210c33a169b66a0e855
scott <scottcooperaus1992@gmail.com>
2023-11-26 16:14:50 +1100

    com

---
+++ dynamicprogramming/Fib.java
@@ -0,0 +1,15 @@
+public class Fib {
+    public static void main(String[] args) {
+        System.out.println(Fib.fib(5));    
+    }
+    public static int fib(int n){
+
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+  
+}
